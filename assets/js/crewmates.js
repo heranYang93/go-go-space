b9f60fca9c515cfc10e17ac9ada75428ca507a9c
hy <yang.hrn@gmail.com>
2021-12-11 19:05:20 +0000

    Updated crewmate related functions and styles

--- assets/js/crewmates.js
+++ assets/js/crewmates.js
@@ -1,61 +1,35 @@
- 
-// break {
-
-
 async function getCrewmatesData() {
   var crewmate1
   // var link = "http://api.open-notify.org/astros.json"
   var link = "https://course-anywhere.herokuapp.com/http://api.open-notify.org/astros.json"
   var temp = await fetch(link)
  
-
-
     .then(function (response) {
       return response.json();
     })
 
     .then(function(data){
-      crewmate1 = data.people[0].name
-      crewmate2 = data.people[1].name
-      crewmate3 = data.people[2].name
-      crewmate4 = data.people[6].name
-      crewmate5 = data.people[7].name
-      crewmate6 = data.people[8].name
-      crewmate7 = data.people[9].name
-      crewmate8 = data.people[3].name
-      crewmate9 = data.people[4].name
-      crewmate10 = data.people[5].name
       
-      document.querySelector("#mate1").textContent =
-        crewmate1;
-        document.querySelector("#mate2").textContent =
-        crewmate2;
-        document.querySelector("#mate3").textContent =
-        crewmate3;
-        document.querySelector("#mate4").textContent =
-        crewmate4;
-        document.querySelector("#mate5").textContent =
-        crewmate5;
-        document.querySelector("#mate6").textContent =
-        crewmate6;
-        document.querySelector("#mate7").textContent =
-        crewmate7;
-        // document.querySelector("#mate8").textContent =
-        // crewmate8;
-        // document.querySelector("#mate9").textContent =
-        // crewmate9;
-        // document.querySelector("#mate10").textContent =
-        // crewmate10;
-        
+      var astronautData = data.people
+      var idToDisplay = 0
 
+      console.log(astronautData)
+
+      for (var i=0; i<astronautData.length; i++){
+        if (astronautData[i].craft === 'ISS'){
+
+          idToDisplay ++
+          console.log()
+          var thisCrewmateEl = document.createElement('li')
+          thisCrewmateEl.innerHTML=`<span id="mate${idToDisplay}">${astronautData[i].name}</span>`
+          document.querySelector('.crewmatelist').append(thisCrewmateEl)
+        }
+      }
     })
 
-    // .catch(function(error){
-    //   alert("Error")
-    // })
-    
-
-
+    .catch(function(error){
+      alert("Crewmate API Error")
+    })
 
 }
 
