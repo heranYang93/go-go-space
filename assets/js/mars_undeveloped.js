d3fb1d9c8d217e200ceb1d3d41f84ebcfa035dc6
hy <yang.hrn@gmail.com>
2021-12-11 16:52:04 +0000

    refactored main page, map script

---
+++ assets/js/mars_undeveloped.js
@@ -0,0 +1,16 @@
+// This script is disabled, whose function is not used in the first version
+// Further development can start from here
+
+// var apiKey = 'z9pRWUcIVzfatJAKeH5sQatsuyXk7ZZsMD9POXUC';
+
+//     function marsWeather () {
+//         fetch(`https://api.nasa.gov/insight_weather/?api_key=${apiKey}&feedtype=json&ver=1.0`)
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             console.log('mars data', data);
+//         })
+//     }
+
+//     marsWeather()
